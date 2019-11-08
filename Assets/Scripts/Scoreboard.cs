bc1b91ded516e15919274b80f0a32e890e744439
dylanmpeck <dylanmpeck@gmail.com>
2019-11-07 23:10:32 -0800

    Baseline for Tutorial

---
+++ Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scoreboard : MonoBehaviour
+{
+    public static int score = 0;
+    public static Text scoreText;
+
+    private void Start()
+    {
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
+    }
+
+    public static void addScore()
+    {
+        score += 1;
+        scoreText.text = score.ToString();
+    }
+}
